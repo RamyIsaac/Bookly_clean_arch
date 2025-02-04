8aa3931799d7f7873cb2fc313341d432e61a2e05
Ramy Isaac <ramyeshak191@gmail.com>
2025-02-04 21:18:50 +0200

    tiny edit

--- lib/features/home/domain/use_cases/fetch_featured_books_use_case.dart
+++ lib/features/home/domain/use_cases/fetch_featured_books_use_case.dart
@@ -1,5 +1,16 @@
+import 'package:bookly_app/core/errors/failures.dart';
+import 'package:bookly_app/core/use_cases/use_case.dart';
+import 'package:bookly_app/features/home/domain/entities/book_entity.dart';
 import 'package:bookly_app/features/home/domain/repos/home_repo.dart';
+import 'package:dartz/dartz.dart';
 
-class FetchFeaturedBooksUseCase {
-  //final  HomeRepo homeRepo;
+class FetchFeaturedBooksUseCase extends UseCase {
+  final HomeRepo homeRepo;
+
+  FetchFeaturedBooksUseCase(this.homeRepo);
+
+  @override
+  Future<Either<Failure, List<BookEntity>>> call() async {
+    return await homeRepo.fetchFeaturedBooks();
+  }
 }
--- lib/features/home/domain/use_cases/fetch_newest_books_use_case.dart
+++ lib/features/home/domain/use_cases/fetch_newest_books_use_case.dart
@@ -1,7 +1,7 @@
 import 'package:bookly_app/core/errors/failures.dart';
+import 'package:bookly_app/core/use_cases/use_case.dart';
 import 'package:bookly_app/features/home/domain/entities/book_entity.dart';
 import 'package:bookly_app/features/home/domain/repos/home_repo.dart';
-import 'package:bookly_app/features/home/domain/use_cases/fetch_featured_books_use_case.dart';
 import 'package:dartz/dartz.dart';
 
 class FetchNewestBooksUseCase extends UseCase {
