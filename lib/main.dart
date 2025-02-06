e393fd0d065f40f16fb27d055af0a3dcf09c693f
Ramy Isaac <ramyeshak191@gmail.com>
2025-02-06 15:48:48 +0200

    create simple bloc observer

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,24 @@
 import 'package:bookly_app/constants.dart';
 import 'package:bookly_app/core/utils/app_router.dart';
+import 'package:bookly_app/core/utils/functions/service_locator.dart';
+import 'package:bookly_app/core/utils/simple_bloc_observer.dart';
+import 'package:bookly_app/features/home/data/repos/home_repo_impl.dart';
 import 'package:bookly_app/features/home/domain/entities/book_entity.dart';
+import 'package:bookly_app/features/home/domain/use_cases/fetch_featured_books_use_case.dart';
+import 'package:bookly_app/features/home/domain/use_cases/fetch_newest_books_use_case.dart';
+import 'package:bookly_app/features/home/presentation/manager/cubit/fetch_featured_books_cubit.dart';
+import 'package:bookly_app/features/home/presentation/manager/cubit/fetch_newest_books_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/adapters.dart';
 
 void main() async {
+  setupServiceLocator();
   await Hive.initFlutter();
   Hive.registerAdapter(BookEntityAdapter());
   await Hive.openBox<BookEntity>(kFeaturedBox);
+  await Hive.openBox<BookEntity>(kNewestBox);
+  Bloc.observer = SimpleBlocObserver();
   runApp(const Bookly());
 }
 
@@ -16,12 +27,26 @@ class Bookly extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      routerConfig: AppRouter.router,
-      theme: ThemeData.dark().copyWith(
-        scaffoldBackgroundColor: kPrimaryColor,
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (context) {
+          return FetchFeaturedBooksCubit(
+              FetchFeaturedBooksUseCase(getIt.get<HomeRepoImpl>()))
+            ..fetchFeaturedBooks();
+        }),
+        BlocProvider(create: (context) {
+          return FetchNewestBooksCubit(
+              FetchNewestBooksUseCase(getIt.get<HomeRepoImpl>()))
+            ..fetchNewestBooks();
+        }),
+      ],
+      child: MaterialApp.router(
+        routerConfig: AppRouter.router,
+        theme: ThemeData.dark().copyWith(
+          scaffoldBackgroundColor: kPrimaryColor,
+        ),
+        debugShowCheckedModeBanner: false,
       ),
-      debugShowCheckedModeBanner: false,
     );
   }
 }
